41ef37c06c286bdf651acbdd9d6745b50f4dfb53
Jelsy Singh <Jelsy Singh@10.120.157.245>
2023-07-14 16:58:36 +0530

    attempt 1

---
+++ MyLoginWeb/src/com/ofss/LoginServelet.java
@@ -0,0 +1,45 @@
+package com.ofss;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServelet
+ */
+public class LoginServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServelet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname=request.getParameter("uname");
+		String pword=request.getParameter("pword");
+		
+		boolean isValidUser=ValidateUser.autheticateUser(uname, pword);
+		
+		if (isValidUser)
+			request.getRequestDispatcher("Success.jsp").forward(request, response);
+		else
+			request.getRequestDispatcher("failure.jsp").forward(request, response);
+
+		//doGet(request, response);
+	}
+
+}
